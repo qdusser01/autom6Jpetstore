10679c25fac7686a3a4749a2cf68e8109892c323
Quentin Dusser <qdusser@NX-DOMAIN.LAN>
2020-07-17 15:16:55 +0200

    correction exercice 2

--- src/test/java/org/seleniumInteractionBasic/PrixPanierTest.java
+++ src/test/java/org/seleniumInteractionBasic/PrixPanierTest.java
@@ -32,7 +32,7 @@ public class PrixPanierTest {
 	}
 
 	@Test
-	public void test() throws Throwable {
+	public void test() throws InterruptedException{
 		driver.get("http://192.168.102.36:8090/jpetstore-1.0.5-env2");
 		logger.info("Accès au magasin");
 		// driver.findElement(By.xpath("//a[@href='shop/signonForm.do']"));
@@ -67,12 +67,12 @@ public class PrixPanierTest {
 		driver.findElement(By.xpath("//font[contains(., 'FI-SW-02')]")).click();
 		driver.findElement(By.xpath("//a[contains(@href, 'addItemToCart')]")).click();
 		
-		WebElement titre_panier=driver.findElement(By.xpath("//*[text() = 'Shopping Catt']"));
+		WebElement titre_panier=driver.findElement(By.xpath("//*[text() = 'Shopping Cart']"));
 		
 		verifElementAffiche(titre_panier, "mon panier n'est pas affiché");
 
-		//SALUUUUT
-
+		Thread.sleep(-6);
+		
 		WebElement champ_quantite= driver.findElement(By.xpath("//input[@type='text']"));
 		ecrireChampTexte(champ_quantite, "2");
 	
@@ -83,7 +83,7 @@ public class PrixPanierTest {
 		float totaltotal = Float.parseFloat(Total);
 		
 		try {
-			assertTrue("Le prix total n'est pas egal au prix unitaire X 2",totaltotal == prixunitaire*3);
+			assertTrue("Le prix total n'est pas egal au prix unitaire X 2",totaltotal == prixunitaire*2);
 		}
 		catch(AssertionError e) {
 			logger.error("Le prix total n'est pas egal au prix unitaire X 2");
